88ac22327d4f9dafd7648faad5579cde81e2a4ee
mmcm2-cin <mmcm2@cin.ufpe.br>
2025-05-26 13:42:22 -0300

    (feat/top-bar): Criação do componente FrontEnd da Top Bar para aplicação web.

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -2,5 +2,6 @@ import LogoCITi from "./Logo.png";
 import LogoCITiPet from "./Logo-CITi-Pet.png"
 import BotaoClose from "./Botão-close.png"
 import addIcon from "./generalIcons/addIcon.png"
+import finalTopBar from "./finalTopBar.png"
 
-export { LogoCITi, LogoCITiPet, BotaoClose, addIcon};
+export { LogoCITi, LogoCITiPet, BotaoClose, addIcon, finalTopBar};
