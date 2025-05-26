119f2db2468db89683634077302278bd340130bb
Breno Augusto Ferreira <155557831+brenoafsa@users.noreply.github.com>
2025-05-26 09:39:38 -0300

    Revert "Adicionado o componente da Top Bar"

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -2,6 +2,5 @@ import LogoCITi from "./Logo.png";
 import LogoCITiPet from "./Logo-CITi-Pet.png"
 import BotaoClose from "./Botão-close.png"
 import addIcon from "./generalIcons/addIcon.png"
-import finalTopBar from "./finalTopBar.png"
 
-export { LogoCITi, LogoCITiPet, BotaoClose, addIcon, finalTopBar};
+export { LogoCITi, LogoCITiPet, BotaoClose, addIcon};
