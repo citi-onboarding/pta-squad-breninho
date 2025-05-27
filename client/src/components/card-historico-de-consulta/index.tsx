d3873c55aa81d7291739eb47376109c6a41714a6
mmcm2-cin <mmcm2@cin.ufpe.br>
2025-05-27 15:49:25 -0300

    (feat/card-historico-de-consulta): Criação do componente front-end referente ao card histórico de consulta

--- client/src/components/card-historico-de-consulta/index.tsx
+++ client/src/components/card-historico-de-consulta/index.tsx
@@ -4,22 +4,22 @@ import { ArrowRight } from "lucide-react";
 interface tipos_das_informacoes {
   date: string;
   time: string;
-  type_of_consultation: string;
+  typeOfConsultation: string;
   doctor: string;
 }
 
-const Card_Historico_de_Consulta: React.FC <tipos_das_informacoes> = ({ date, time, type_of_consultation, doctor }) => {
+const Card_Historico_de_Consulta: React.FC <tipos_das_informacoes> = ({ date, time, typeOfConsultation, doctor }) => {
     return (
-    <div className = "flex items-center justify-between bg-[#F0F0F0] rounded-xl w-[510px] h-[82px] gap-[61px] p-[16px] text-[14px]">
-        <div className = "bg-white p-4 rounded-md text-center w-[51px] h-[50px] shadow-sm flex flex-col items-center justify-center">
-            <div className = "font-bold">{date}</div>
-            <div className = "font-bold">{time}</div>
+    <div className="flex items-center justify-between bg-[#F0F0F0] rounded-xl w-[510px] h-[82px] gap-[61px] py-[16px] px-[24px] text-[14px]">
+        <div className="bg-white p-4 rounded-md text-center w-[51px] h-[50px] shadow-sm flex flex-col items-center justify-center">
+            <div className="font-bold">{date}</div>
+            <div className="font-bold">{time}</div>
         </div>
 
-        <div className = "text-sm font-bold">{type_of_consultation} </div>
-        <div className = "text-sm">{doctor}</div>
+        <div className="font-bold w-[115px] whitespace-nowrap">{typeOfConsultation} </div>
+        <div className="w-[90px] whitespace-nowrap">{doctor}</div>
         
-        <button className = "w-[24px] h-[24px] flex items-center justify-center border-b-2 border-transparent hover:border-[#50E678]">
+        <button className="w-[24px] h-[24px] flex items-center justify-center">
             <ArrowRight/>
         </button>
     </div>
