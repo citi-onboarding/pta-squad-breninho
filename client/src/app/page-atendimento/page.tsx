838e14e138c7a00a9b656bd29c1e5f2aa7fe0470
Gabriel Cássio <gabriel.cgc.contato@gmail.com>
2025-05-28 12:55:31 -0300

    feat(pag-atendiemnto): adds the service page with pet cards to consult

--- client/src/app/page-atendimento/page.tsx
+++ client/src/app/page-atendimento/page.tsx
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon, CalendarIcon, PlusCircledIcon } from "@radix-ui/react-icons";
 import TopBar from "@/components/top-bar"
 import CardConsulta from "@/components/card-consulta";
 import { Button } from "@/components/ui/button";
@@ -7,24 +8,57 @@ const PageAtendimento = () => {
         <body>
             {/* Top bar */}
             <TopBar></TopBar>
-            <section>
+            <section className="flex flex-col justify-center pt-[48px] pr-[194px] pb-[76px] pl-[194px] gap-[32px]">
                 {/* Call back page Atendimento */}
-                <div></div>
+                <div className="flex flex-row items-center h-[53px] w-[365px] gap-[16px]">
+                    <ChevronLeftIcon className="h-[32px] w-[32px]"></ChevronLeftIcon>
+                    <p className="text-[48px] font-bold font-[Source Code Pro]">Atendimento</p>
+                </div>
                 {/* Search components */}
-                <div></div>
+                <div className="flex flex-col gap-[24px]">
+                    <p className="text-[24px] font-normal leading-[110.00000000000001%]">Qual é o médico?</p>
+                    <div className="flex flex-row items-center h-[50x] w-[660px] gap-[24px]">
+                        <input className="h-[50px] w-[520px] rounded-[8px] border-[#101010] border-[1px] p-[16px] gap-[16px]" type="text" placeholder="Pesquise aqui..."/>
+                        <Button className="h-[42px] w-[116px] rounded-[24px] py-[12px] px-[32px] bg-[#7D1AD7]">Buscar</Button>
+                    </div>
+                </div>
                 {/* Organizer components */}
-                <div></div>
+                <div className="flex flex-row justify-between">
+                    <div className="flex flex-row items-center justify-center h-[58px] w-[243px] rounded-[12px] p-[8px] gap-[8px] bg-[#F0F0F0] ">
+                        <Button variant="link" className="flex items-center justify-center h-[42px] w-[92px] rounded-[8px] bg-white text-[#101010] text-[16px] font-normal"><p>Histórico</p></Button>
+                        <Button variant="link" className="flex items-center justify-center h-[42px] w-[127px] text-[#101010] text-[16px] font-normal"><p>Agendamento</p></Button>
+                    </div>
+                    <div className="flex flex-row h-[56px] w-[268px] gap-[16px]">
+                        <Button className="flex flex-row items-center justify-center h-[56px] w-[126px] bg-white border-[1px] border-[#D9D9D9] gap-[8px] py-[16px] px-[12px] rounded-[8px] text-[#101010] text-[16px] font-normal">
+                            <p>dd/mm/aa</p>
+                            <CalendarIcon></CalendarIcon>
+                        </Button>
+                        <Button className="flex flex-row items-center justify-center h-[56px] w-[126px] bg-white border-[1px] border-[#D9D9D9] gap-[8px] py-[16px] px-[12px] rounded-[8px] text-[#101010] text-[16px] font-normal">
+                            <p>dd/mm/aa</p>
+                            <CalendarIcon></CalendarIcon>
+                        </Button>
+                    </div>
+                </div>
                 {/* Status Pet cards */}
-                <div>
-                    <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" textStatus="Primeira Consulta"></CardConsulta>
-                    <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" textStatus="Retorno"></CardConsulta>
-                    <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" textStatus="Check-up"></CardConsulta>
-                    <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" textStatus="Vacinação"></CardConsulta>
-                    <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" textStatus="Vacinação"></CardConsulta>
-                    <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" textStatus="Primeira Consulta"></CardConsulta>
+                <div className="flex flex-col gap-[24px]">
+                    <div className="flex flex-row gap-[24px]">
+                        <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" status="Primeira Consulta"></CardConsulta>
+                        <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" status="Retorno"></CardConsulta>
+                        <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" status="Check-up"></CardConsulta>
+                    </div>
+                    <div className="flex flex-row gap-[24px]">
+                        <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" status="Vacinação"></CardConsulta>
+                        <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" status="Vacinação"></CardConsulta>
+                        <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" status="Primeira Consulta"></CardConsulta>
+                    </div>
                 </div>
                 {/* New Consults Button */}
-                <Button></Button>
+                <div className="flex justify-end w-full mt-[153px]">
+                    <Button className="h-[48px] w-[205px] bg-[#50E678] rounded-[24px] py-[12px] px-[32px] gap-[10px] shadow-[0 4px 4px #0000001A]">
+                        <PlusCircledIcon></PlusCircledIcon>
+                        Nova Consulta
+                    </Button>
+                </div>
             </section>
         </body>
     )
