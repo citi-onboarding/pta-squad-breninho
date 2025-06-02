69cb168b6d6d81aa7bc93e086b9d28f6ce6b1e3b
Gabriel Cássio <gabriel.cgc.contato@gmail.com>
2025-06-02 08:39:03 -0300

    feat(rotas-site): Adds the function of routing existing pages

--- client/src/app/page-atendimento/page.tsx
+++ client/src/app/page-atendimento/page.tsx
@@ -7,8 +7,63 @@ import { Button } from "@/components/ui/button";
 import { ArrowBack, CalendarMonth, PlusCircled } from "@/assets";
 import { useRouter } from "next/navigation";
 
+
+// Para construir um roteamento lógico e funcional: 
+// 1º -> Passar o id do animal no card
+// [
+//      [ A, B, C],
+//      [ D, E, F],
+//      [ G, H, I],
+// ]
+// 2º -> O id será captado e processado
+// 3º -> Renderização das informações da página de consulta
+
+// ---------------
+interface interfProps {
+    id: number,
+    date: string,
+    time: string,
+    nameOwner: string,
+    namePet: string,
+    nameDoctor: string,
+    imageAnimal: string,
+    status: "Primeira Consulta" | "Check-up" | "Retorno" | "Vacinação" | ""
+}
+const cardsConsultas: interfProps[] = [
+    {id: 1, date: "18/02", time: "13:00", nameOwner: "João Alves", namePet: "Luna", nameDoctor: "Dr. José Carlos", imageAnimal: "gato", status: "Primeira Consulta" },
+    {id: 2, date: "18/02", time: "13:00", nameOwner: "João Alves", namePet: "Luna", nameDoctor: "Dr. José Carlos", imageAnimal: "gato", status: "Retorno" },
+    {id: 3, date: "18/02", time: "13:00", nameOwner: "João Alves", namePet: "Luna", nameDoctor: "Dr. José Carlos", imageAnimal: "gato", status: "Check-up" },
+    {id: 4, date: "18/02", time: "13:00", nameOwner: "João Alves", namePet: "Luna", nameDoctor: "Dr. José Carlos", imageAnimal: "gato", status: "Vacinação" },
+    {id: 5, date: "18/02", time: "13:00", nameOwner: "João Alves", namePet: "Luna", nameDoctor: "Dr. José Carlos", imageAnimal: "gato", status: "Vacinação" },
+    {id: 6, date: "18/02", time: "13:00", nameOwner: "João Alves", namePet: "Luna", nameDoctor: "Dr. José Carlos", imageAnimal: "gato", status: "Primeira Consulta" },
+];
+// Organizar os Ids nos cards
+
 const PageAtendimento = () => {
+    // Instace of AppRouter
     const router = useRouter();
+    // Create a automatic row card creator 
+    const consultId = (posArray: number) => {
+        return cardsConsultas.slice(posArray, posArray+3);
+    };
+    const renderCards = (objCards: interfProps[]) => {
+        return(
+            objCards.map((consulta: interfProps) => 
+                (
+                    <CardConsulta key={consulta.id} date={consulta.date} time={consulta.time} nameOwner={consulta.nameOwner} namePet={consulta.namePet} nameDoctor={consulta.nameDoctor} imageAnimal={consulta.imageAnimal} status={consulta.status} onClick={() => router.push(`/page-detalhes-consulta/${consulta.id}`)}></CardConsulta>
+                )
+            )
+        )
+    }
+    const consultRows = cardsConsultas.map((consulta, index) =>{
+        if ((index % 3) === 0 ){
+            const cards = consultId(index);
+            return(
+                <div key={consulta.id} className="flex flex-row gap-[24px]">{renderCards(cards)}</div>
+            )
+        }
+    });
+    // ----------------------------------------------
     return(
         <body className="flex flex-col items-center">
             {/* Top bar */}
@@ -32,32 +87,23 @@ const PageAtendimento = () => {
                 {/* Organizer components */}
                 <div className="flex flex-row justify-between">
                     <div className="flex flex-row items-center justify-center h-[58px] w-[243px] rounded-[12px] p-[8px] gap-[8px] bg-[#F0F0F0] ">
-                        <Button variant="link" className="flex items-center justify-center h-[42px] w-[92px] rounded-[8px] bg-white text-[#101010] text-[16px] font-normal hover:no-underline hover:bg-[#D9D9D9]" onClick={() => '/page-historico'}><p>Histórico</p></Button>
-                        <Button variant="link" className="flex items-center justify-center h-[42px] w-[127px] text-[#101010] text-[16px] font-normal hover:no-underline hover:bg-[#D9D9D9]" onClick={() => '/page-agendamento'}><p>Agendamento</p></Button>
+                        <Button variant="link" className="flex items-center justify-center h-[42px] w-[92px] rounded-[8px] bg-white text-[#101010] text-[16px] font-normal hover:no-underline hover:bg-[#D9D9D9]" ><p>Histórico</p></Button>
+                        <Button variant="link" className="flex items-center justify-center h-[42px] w-[127px] text-[#101010] text-[16px] font-normal hover:no-underline hover:bg-[#D9D9D9]"><p>Agendamento</p></Button>
                     </div>
                     <div className="flex flex-row h-[56px] w-[268px] gap-[16px]">
                         <Button className="flex flex-row items-center justify-center h-[56px] w-[126px] bg-white border-[1px] border-[#D9D9D9] gap-[8px] py-[16px] px-[12px] rounded-[8px] text-[#101010] text-[16px] font-normal hover:bg-[#D9D9D9]">
-                            <p>dd/mm/aa</p>
+                            <p>{"dd/mm/aa"}</p>
                             <Image src={CalendarMonth} alt="Calendar Month"></Image>
                         </Button>
                         <Button className="flex flex-row items-center justify-center h-[56px] w-[126px] bg-white border-[1px] border-[#D9D9D9] gap-[8px] py-[16px] px-[12px] rounded-[8px] text-[#101010] text-[16px] font-normal hover:bg-[#D9D9D9]">
-                            <p>dd/mm/aa</p>
+                            <p>{"dd/mm/aa"}</p>
                             <Image src={CalendarMonth} alt="Calendar Month"></Image>
                         </Button>
                     </div>
                 </div>
                 {/* Status Pet cards */}
                 <div className="flex flex-col gap-[24px]">
-                    <div className="flex flex-row gap-[24px]">
-                        <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" status="Primeira Consulta"></CardConsulta>
-                        <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" status="Retorno"></CardConsulta>
-                        <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" status="Check-up"></CardConsulta>
-                    </div>
-                    <div className="flex flex-row gap-[24px]">
-                        <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" status="Vacinação"></CardConsulta>
-                        <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" status="Vacinação"></CardConsulta>
-                        <CardConsulta date="18/02" time="13:00" nameOwner="João Alves" namePet="Luna" nameDoctor="Dr. José Carlos" status="Primeira Consulta"></CardConsulta>
-                    </div>
+                    {consultRows}
                 </div>
                 {/* New Consults Button */}
                 <div className="flex justify-end w-full mt-[153px]">
@@ -67,7 +113,6 @@ const PageAtendimento = () => {
                     </Button>
                 </div>
             </section>
-            
         </body>
     )
 };
