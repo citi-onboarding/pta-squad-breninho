5453dc1d7142ccff655add7765547ecd17fc6e1f
Cássio <cdrl@cin.ufpe.br>
2025-06-02 11:54:33 -0300

    fix(card-verde-mobile): Corrigido o width do componente

--- mobile/src/components/card-verde-mobile/index.tsx
+++ mobile/src/components/card-verde-mobile/index.tsx
@@ -3,7 +3,7 @@ import { HomeIndicator } from "@assets";
 
 export default function CardVerdeMobile() {
   return (
-    <View className="w-[414px] h-[75px] bg-[#50E678] rounded-tl-[24px] rounded-tr-[24px] justify-center items-center" >
+    <View className="w-full h-20 bg-[#50E678] rounded-t-[24px] justify-center items-center" >
         <Image source={HomeIndicator} className="w-[134px] h-[5px] inset-y-[15px]" />
     </View>
   );
