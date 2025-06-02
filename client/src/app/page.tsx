69f81e3ffc0a2b8e6c626248635f2298d098f061
Gabriel Cássio <gabriel.cgc.contato@gmail.com>
2025-06-02 11:47:57 -0300

    fix(rotas-site): fix the service card to ajust routes

--- client/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// import Image from "next/image";
-// import { LogoCITi } from "../assets";
-import CardConsulta from "@/components/card-consulta";
-
-export default function Home() {
-  return (
-    <CardConsulta date="27/05" time="10:00" nameOwner="Breno" namePet="Gatuso" nameDoctor="Doutor Marcos" status="Vacinação"/>
-  );
-}
--- client/src/components/top-bar/index.tsx
+++ client/src/components/top-bar/index.tsx
@@ -1,21 +1,25 @@
+'use client'
+
 import React from 'react';
 import { finalTopBar } from '@/assets';
 import { LogoCITiPet } from '@/assets';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 function TopBar() {
+    const router = useRouter();
     return (
         <div 
             className = "w-full bg-white flex items-center justify-center border-b border-[#D9D9D9] h-[114px] pl-[48px] pr-[48px] pt-[20px] pb-[20px]">
             <div className = "flex flex-1 justify-center items-center gap-20">
-                <button className = "text-lg text-black hover:underline decoration-[#50E678] decoration-2 w-[86px] h-[42px] flex items-center justify-center"> Atendimento </button>
+                <button className = "text-lg text-black hover:underline decoration-[#50E678] decoration-2 w-[86px] h-[42px] flex items-center justify-center" onClick={() => router.push('/page-atendimento')}> Atendimento </button>
                 
                 <Image className = "w-[189px] h-[74px]"
                     src={LogoCITiPet}
                     alt="Logo CITiPet"
                 />
                 
-                <button className = "text-lg text-black hover:underline decoration-[#50E678] decoration-2 w-[62px] h-[42px] flex items-center justify-center"> Cadastro </button>
+                <button className = "text-lg text-black hover:underline decoration-[#50E678] decoration-2 w-[62px] h-[42px] flex items-center justify-center" onClick={() => router.push('/page-cadastro')}> Cadastro </button>
             </div>
             
             <div className = "absolute right-0 px-[48px]">
