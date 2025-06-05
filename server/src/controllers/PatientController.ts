ed46e7c10dcf2d0cf1a1fed88e73224d7dd8c434
Cássio <cdrl@cin.ufpe.br>
2025-06-05 14:29:44 -0300

    feat:(crud-consulta): Implementação de CRUD Patient e rotas

---
+++ server/src/controllers/PatientController.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from "express";
+import { Citi, Crud } from "../global";
+
+class PatientController implements Crud {
+  constructor(private readonly citi = new Citi("Patient")) {}
+
+  // Create a new patient
+  create = async (req: Request, res: Response) => {
+    const { name, tutorName, age, species } = req.body;
+
+    const isAnyUndefined = this.citi.areValuesUndefined(
+      name,
+      tutorName,
+      age,
+      species
+      //appointments --> Comentado, nao eh necessario
+    );
+    if (isAnyUndefined) {
+      return res.status(400).send({ message: "All fields are required." });
+    }
+
+    const newPatient = { name, tutorName, age, species };
+    const { httpStatus, message } = await this.citi.insertIntoDatabase(newPatient);
+
+    return res.status(httpStatus).send({ message });
+  };
+
+  // Get all patients
+  get = async (req: Request, res: Response) => {
+    const { httpStatus, values } = await this.citi.getAll();
+    return res.status(httpStatus).send(values);
+  };
+
+  // Get patient by ID
+  getById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { httpStatus, value } = await this.citi.findById(Number(id));
+    return res.status(httpStatus).send(value);
+  };
+
+  // Update patient
+  update = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const updatedPatient = req.body;
+    const { httpStatus, messageFromUpdate } = await this.citi.updateValue(Number(id), updatedPatient);
+    return res.status(httpStatus).send({ messageFromUpdate });
+  };
+
+  // Delete patient
+  delete = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { httpStatus, messageFromDelete } = await this.citi.deleteValue(Number(id));
+    return res.status(httpStatus).send({ messageFromDelete });
+  };
+}
+
+export default new PatientController();
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import userController from "./controllers/UserController";
 import appointmentController from "./controllers/ConsultaController";
+import PatientController from "./controllers/PatientController";
 
 const routes = express.Router();
 
@@ -10,7 +11,19 @@ routes.delete("/user/:id", userController.delete);
 routes.patch("/user/:id", userController.update);
 
 routes.post("/page-cadastro", appointmentController.create);
+
 routes.get("/page-atendimento", appointmentController.get);
+
+routes.post("/page-detalhes-consulta", appointmentController.getById);
 routes.get("/page-detalhes-consulta", appointmentController.get);
+routes.get("/page-detalhes-consulta/:id", appointmentController.getById);
+routes.patch("/page-detalhes-consulta/:id", appointmentController.update);
+routes.delete("/page-detalhes-consulta/:id", appointmentController.delete);
+
+routes.post("/page-paciente", PatientController.create);
+routes.get("/page-paciente", PatientController.get);
+routes.get("/page-paciente/:id", PatientController.getById);
+routes.patch("/page-paciente/:id", PatientController.update);
+routes.delete("/page-paciente/:id", PatientController.delete);
 
 export default routes;
