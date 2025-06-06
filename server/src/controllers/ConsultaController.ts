f8c3ffb7d14bd95cfcd18af7bdf2cad483e6e487
Gabriel Cássio <gabriel.cgc.contato@gmail.com>
2025-06-06 20:53:24 -0300

    feat(integração-cadastro):adds-all-the integration-pages

--- server/src/controllers/ConsultaController.ts
+++ server/src/controllers/ConsultaController.ts
@@ -2,9 +2,6 @@ import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 import { Citi, Crud } from "../global";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
 
 
 class AppointmentController implements Crud {
